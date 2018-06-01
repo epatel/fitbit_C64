3a68bb235005f15af9db542466a0b470737675b8
Michel de Bree <michel@micheldebree.nl>
2018-06-01 13:19:46 +0200

    Centered time (on Versa). Optimized code.

--- app/index.js
+++ app/index.js
@@ -1,45 +1,23 @@
-import { me } from "appbit";
-import clock from "clock";
-import document from "document";
-import * as util from "./utils";
+import clock from 'clock';
+import document from 'document';
 
-// TIME
-let separator = document.getElementById("separator");
-let hours1 = document.getElementById("hours1");
-let hours2 = document.getElementById("hours2");
-let mins1 = document.getElementById("mins1");
-let mins2 = document.getElementById("mins2");
+let separator = document.getElementById('separator');
+let hours1 = document.getElementById('hours1');
+let hours2 = document.getElementById('hours2');
+let mins1 = document.getElementById('mins1');
+let mins2 = document.getElementById('mins2');
 
-clock.granularity = "seconds";
+clock.granularity = 'seconds';
 
 clock.ontick = evt => {
   let d = evt.date;
-
-  // HOURS
-  let hours = d.getHours();
-  hours = util.zeroPad(hours);
-
-  setHours(hours);
-
-  // MINUTES
-  let minute = ("0" + d.getMinutes()).slice(-2);
-  setMins(minute);
-
-  // BLINK SEPARATOR
-  setSeparator(d.getSeconds());
-}
-
-// Blink time separator
-function setSeparator(val) {
-  separator.style.display = (val % 2 === 0 ? "inline" : "none");
-}
+  setHours(d.getHours());
+  setMins(d.getMinutes());
+  separator.style.display = d.getSeconds() % 2 === 0 ? 'inline' : 'none';
+};
 
 function setHours(val) {
-  if (val > 9) {
-    drawDigit(Math.floor(val / 10), hours1);
-  } else {
-    drawDigit("", hours1);
-  }
+  drawDigit(Math.floor(val / 10), hours1);
   drawDigit(Math.floor(val % 10), hours2);
 }
 
@@ -54,5 +32,5 @@ function setDate(val) {
 }
 
 function drawDigit(val, place) {
-  place.image = `${val+48}.png`;
+  place.image = `${val + 48}.png`;
 }
